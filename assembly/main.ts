24d302b03a26f82cf547e7de19ba3215e1dec2b9
Oleh Makarenko <olegomm@gmail.com>
2019-09-22 00:12:46 +0300

    Methods added to contract

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,11 +1,13 @@
 //@nearfile
-import { context, storage, logging, collections, PersistentMap } from "near-runtime-ts";
+import { context, storage, logging, collections, PersistentMap, PersistentTopN } from "near-runtime-ts";
+//import "allocator/arena";
+// Import encoder
+import { JSONEncoder } from "assemblyscript-json";
 
 // --- contract code goes below
 
 let balances = new PersistentMap<string, u64>("b:");
 let approves = new PersistentMap<string, u64>("a:");
-//let sponsors = new PersistentMap<string, i32>("d:");
 
 
 let TOTAL_SUPPLY: u64 = 1000000;
@@ -69,12 +71,16 @@ function getBalance(owner: string) : u64 {
 }
 
 
-////HT
 
 
 
-//let sponsors = new PersistentMap<string, u64>();
-//let judges = new PersistentMap<string, u64>();
+
+
+////
+////SMART HACKATHON
+////
+
+
 
 
 
@@ -82,10 +88,13 @@ function getBalance(owner: string) : u64 {
 let sponsors = new PersistentMap<string, i32>("s:");
 let judges = new PersistentMap<string, i32>("j:");
 let members = new PersistentMap<string, i32>("m:");
+//let members = new PersistentTopN<i32>("m:");
+let totalAmount = 0;
 
 
 export function setSponsor(address: string, amount: i32): void {
   storage.set<i32>("s:" + address, amount);
+  totalAmount += amount;
 }
 
 export function getSponsor(address: string): i32 {
@@ -117,33 +126,72 @@ export function setMemberScore(address: string, score: i32): void {
 }
 
 export function getMemberScore(address: string): i32 {
-   let judge = storage.getPrimitive<i32>("m:" + address, 0)
-   return judge;
-}
-
-
-
-
-export function finalize(address: string): i32 {
-   let judge = storage.getPrimitive<i32>("m:" + address, 0)
-   return judge;
+  let judge = storage.getPrimitive<i32>("m:" + address, 0)
+  return judge;
 }
 
 
 
 
 
+function getAmmountForWinner(place: i32): i32 {
+  let prize: i32;
+  if(place>3) 
+    prize = totalAmount / 2;
+  else 
+    prize = totalAmount;
+
+  totalAmount -= prize;
+  return prize;
+}
+
+class Winner {
+    address: string; 
+    //score: i32; 
+    amount: i32 
+}
+
+
+
+export function finalize(address: string): String {
+
+  let top3Members: Array<string>;  // Array of top 3 accounts. Have to be get from Map "members". 
+
+  //TODO sort and put addresses to Array "top3Mambers"
+
+
+  //let top3 = members.getTop(3);
+  let winners: Winner[];
+
+  for(let i = 0; i < top3Members.length; i++)
+   winners[i]=({
+       address: <string>top3Members[i],
+       //score: <i32>members.get(top3Members[i]),
+       amount: <i32>(getAmmountForWinner(i+1))
+     })
+
+  let encoder = new JSONEncoder();
+
+
+  //TODO put data to JSON string and retun it
+
+
+
+  // Construct necessary object
+  // encoder.pushObject("obj");
+  // for(let i in topMembers)
+  //   if(topMembers[0].address)
+  //     encoder.setString(""+topMembers[i].address, ""+topMembers[i].score);
+  // encoder.popObject();
+
+  // // Get serialized data
+  // let json: Uint8Array = encoder.serialize();
+
+  // // Or get serialized data as string
+  // let jsonString: String = encoder.toString();
+  return "{JSON}";
+}
 
 
 
 
-
-
-// export function setSponsor(address: string, amount: i32): boolean {
-//   storage.setString(address, <string>amount);
-//   return true;
-// }
-
-// export function getSponsor(address: string): string | null {
-//   return storage.getString(address);
-// }
